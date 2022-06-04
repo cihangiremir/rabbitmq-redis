f22cd239fc5c3eb70a0238d58a5073e517d6e2a0
Emirhan Cihangir <cihangiremir@outlook.com>
2022-06-04 15:53:09 +0300

    worker performance optimizations.

--- RabbitMQWorker/Worker.cs
+++ RabbitMQWorker/Worker.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-using System;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,44 +12,49 @@ namespace RabbitMQConsumerWorker
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
+        private IConnection _connection;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            Console.WriteLine("Kuyruk Dinleniyor...");
-            while (!stoppingToken.IsCancellationRequested)
+            CreateConnection();
+            CreateQueue();
+            ConsumeQueue();
+            await Task.CompletedTask;
+        }
+        private void CreateConnection()
+        {
+            _logger.LogInformation("Creating rabbitmq connection");
+            var factory = new ConnectionFactory()
             {
-                string message = "";
-                var factory = new ConnectionFactory() { HostName = "localhost" };
-                using (var connection = factory.CreateConnection())
-                using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare(queue: "user",
-                                        durable: false,
-                                        exclusive: false,
-                                        autoDelete: false,
-                                        arguments: null);
+                HostName = _configuration.GetValue<string>("RabbitMQHost"),
+            };
+            _connection = factory.CreateConnection();
+            _logger.LogInformation("Created rabbitmq connection");
+        }
+        private void CreateQueue()
+        {
+            var model = _connection.CreateModel();
+            model.QueueDeclare("message", false);
+        }
+        private void ConsumeQueue()
+        {
+            _logger.LogInformation("Listening message queue...");
+            string message = "";
 
-                    var consumer = new EventingBasicConsumer(channel);
-                    consumer.Received += (model, ea) =>
-                    {
-                        message = Encoding.UTF8.GetString(ea.Body.ToArray());
-                    };
-                    channel.BasicConsume(queue: "user",
-                                         autoAck: true,
-                                         consumer: consumer);
-                }
-                if (message != "")
-                {
-                    Console.WriteLine("Mesaj Kuyruktan Alındı...");
-                    Console.WriteLine(message);
-                }
-                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(100, stoppingToken);
-            }
+            var channel = _connection.CreateModel();
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                _logger.LogInformation("Message:{@message}", message);
+            };
+            channel.BasicConsume(queue: "message", autoAck: false, consumer: consumer);
         }
     }
 }
